c178deb3f3f64a41c6f1f5033b582e76c8a22959
ThinksSo <stpt.mail@yandex.ru>
2023-05-21 19:50:58 +0300

    Add dice probability

--- JAVA/demo/tasks.java
+++ JAVA/demo/tasks.java
@@ -6,13 +6,15 @@ import java.util.List;
 public class tasks {
 
     public static void main(String[] args) {
+        
         int dividend = 12;
-        System.out.printf("\n\u001B[1mДопустимые делители для числа %d \u001B[0m \n", dividend);
+        System.out.printf("\n\u001B[1mДопустимые делители для числа %s \u001B[0m \n", dividend);
         List<Integer> availableDivider = availableDivider(dividend);
         for (Integer integer : availableDivider) {
             System.out.print(integer + " ");
         }
         System.out.println("\n");
+
 
         int simple = 6;
         System.out.printf("\n\u001B[1mСписок простых чисел до числа %s \u001B[0m \n", simple);
@@ -21,10 +23,14 @@ public class tasks {
             System.out.print(integer + " ");
         }
         System.out.println("\n");
+
+        int givenNum = 10;
+        System.out.printf("\n\u001B[1mВеротность выпадения числа %s \u001B[0m \n", givenNum); 
+        System.out.print(findSum(givenNum));
+        System.out.println("\n");
     }
 
     // Допустимые делители для числа
-
     public static List<Integer> availableDivider(int number) {
         int counter = 0;
         List<Integer> result = new ArrayList<>();
@@ -39,7 +45,6 @@ public class tasks {
     }
 
     // Простые числа
-
     public static List<Integer> findsimpleNumbers(int max) {
         int counter = 0;
         List<Integer> result = new ArrayList<>();
@@ -49,7 +54,7 @@ public class tasks {
                 counter++;
                 if (i % j == 0) {
                     simple = false;
-                    //break;
+                    // break;
                 }
             }
             if (simple) {
@@ -60,4 +65,22 @@ public class tasks {
         System.out.println("Counter (find simple Numbers) = " + counter);
         return result;
     }
+
+    // Для трех шестигранных кубиков
+    public static double findSum(int sum) {
+        int count = 0;
+        int successResult = 0;
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 6; j++) {
+                for (int k = 1; k < 6; k++) {
+                    if (i + j + k == sum) {
+                        successResult++;
+                    }
+                    count++;
+                }
+            }
+        }
+        return ((double) successResult) / ((double) count);
+    }
+
 }
